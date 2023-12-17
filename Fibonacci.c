a43959e790ef63d0d5bf4f3e128cedda471e7ed3
Rajani Akther <151865891+rajani-cell@users.noreply.github.com>
2023-12-18 03:15:26 +0600

    Create Fibonacci.c
    Done

---
+++ Fibonacci.c
@@ -0,0 +1,23 @@
+//Fibonacci.c
+
+
+#include <stdio.h>
+
+int main()
+{
+    int n[51],i,num;
+
+    scanf("%d", &num);
+
+    n[0]=1;
+    n[1]=1;
+
+    for(i=2; i<num; i++)
+    {
+        n[i]=n[i-1]+n[i-2];
+    }
+
+    printf("%d\n",n[num-1]);
+
+
+}
