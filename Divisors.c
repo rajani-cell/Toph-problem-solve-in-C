9fe8f02850442bc7b32ec5c009ea1837b0ae7b9a
Rajani Akther <151865891+rajani-cell@users.noreply.github.com>
2023-12-18 02:43:58 +0600

    Create Divisors.c
    Done

---
+++ Divisors.c
@@ -0,0 +1,20 @@
+//Divisors
+
+
+#include <stdio.h>
+
+int main() {
+
+	int A;
+	scanf("%d",&A);
+	for(int i=1;i<=A;i++)
+		{
+			 if(A%i==0)
+			 {
+				 printf("%d\n",i);
+			 }
+
+		}
+	
+	return 0;
+}
